c861813465075c2944ed0308694f01940654fbd7
longduongxd <longduongxd@gmail.com>
2020-05-09 18:41:05 +0700

    update

---
+++ src/SourceProductInformation/WriteProductInfomation.java
@@ -0,0 +1,56 @@
+package SourceProductInformation;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WriteProductInfomation {
+    public void splitPrice(String price){
+        if()
+    }
+
+    public void getProductInformation(String linkContent, String linkFileText){
+//        try {
+//            URL url = new URL(linkContent);
+//            File file = new File(linkFileText);
+//            if(!file.exists()) throw new FileNotFoundException();
+//            FileWriter fileWriter = new FileWriter(file);
+//            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//            Scanner scan = new Scanner(url.openStream());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try{
+            URL url = new URL(linkContent);
+            File file = new File(linkFileText);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            Byte[] bytes = new Byte[2048];
+            Scanner scan = new Scanner(url.openStream());
+            scan.useDelimiter("\\Z");
+            String content = scan.next();
+            content = content.replaceAll("\n", "");
+            Pattern patternSourceLinkProduct = Pattern.compile("</div><h6><a href=\"(.+?)\">");
+            Pattern patternNameAndPriceProduct = Pattern.compile(".html\">(.+?)</a></h6><div class=\"(.+?)\">" +
+                    "<span(.+?)class=\"price\">(.+?)</span>");
+            Matcher link = patternSourceLinkProduct.matcher(content);
+            Matcher NameAndPrice = patternNameAndPriceProduct.matcher(content);
+            int count = 0;
+            while (link.find() && NameAndPrice.find()){
+                count++;
+                System.out.println(count + "," + NameAndPrice.group(1)+ "," + NameAndPrice.group(4)+ "," + link.group(1));
+                String productInformation = NameAndPrice.group(1)+ "," + NameAndPrice.group(4)+ "," + link.group(1);
+                fileOutputStream.write();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
